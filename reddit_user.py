26974aadcf0f4cabff48bd9b926427971571fac2
adewolff <30436922+adewolff@users.noreply.github.com>
2018-04-17 15:08:12 -0700

    added current project file

--- reddit_user.py
+++ reddit_user.py
@@ -1,9 +1,12 @@
 '''imports a reddit user's .json file and can parse for post/comment info.'''
 
-import requests, json, logging
+import requests
+import json
+import logging
 
-logging.basicConfig(filename='error-log.txt', level=logging.DEBUG, 
-format='%(asctime)s - %(levelname)s - %(message)s')
+logging.basicConfig(filename='error-log.txt', level=logging.DEBUG,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 class reddituser(object):
 
@@ -17,7 +20,7 @@ class reddituser(object):
         '''
         try:
             with requests.get("http://reddit.com/user/{}.json".format(self.username),
-                             headers={"user-agent": "karmacompbot"}) as r:
+                              headers={"user-agent": "karmacompbot"}) as r:
                 r.raise_for_status()
                 return(json.loads(r.text))
             # json_data = json.loads(f.read().decode())
@@ -30,18 +33,18 @@ class reddituser(object):
                 return 429
             else:
                 return None
-                logging.debug('importer HTTP Error: {}'.format(e))
+                logging.critical('importer HTTP Error: {}'.format(e))
 
         ## For using a local .json file:
         # with open('{}.json'.format(self.username)) as json_data:
         #     d = json.load(json_data)
         #     return(d)
-        
+
     def karma(self, submission_type):
         '''returns the karma for the most recent link posted'''
         if submission_type.lower() in {"comment", "comments"}:
             vector = "t1"
-        elif submission_type.lower() in {"link","links"}:
+        elif submission_type.lower() in {"link", "links"}:
             vector = "t3"
         i = 0
         type = self['data']['children'][i]['kind']
@@ -50,11 +53,28 @@ class reddituser(object):
             i += 1
             if i >= 25:
                 return None
-                logging.debug('Parsing error: No {} found in recent profile history.'.format(submission_type))
+                logging.debug(
+                    'Parsing error: No {} found in recent profile history.'.format(submission_type))
             continue
         return self['data']['children'][i]['data']['score']
 
-
-
-        
-
+    def content(self, submission_type):
+        '''returns content for most recent link/comment posted'''
+        if submission_type.lower() in {"comment", "comments"}:
+            vector = "t1"
+        elif submission_type.lower() in {"link", "links"}:
+            vector = "t3"
+        i = 0
+        type = self['data']['children'][i]['kind']
+        if vector == "t1":
+                while type != vector:
+                    type = self['data']['children'][i]['kind']
+                    i += 1
+                    if i >= 25:
+                        return None
+                        logging.debug(
+                                    'Parsing error: No {} found in recent profile history.'.format(submission_type))
+                    continue
+        if self['data']['children'][i]['data']['body'] in "self.":
+            return(['data:']) self['data']['children'][i]['data']['selftext']
+        elif self['data']['children'][i]['data']['body'] not in "self.":
